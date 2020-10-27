7fab8060712ce7adfa77f06292e728166c17fcac
Daniel <dwantula@gmail.com>
2020-10-27 20:42:27 +0100

    improved-code-functions

--- src/screens/Calculator/Calculator.jsx
+++ src/screens/Calculator/Calculator.jsx
@@ -3,7 +3,6 @@ import CalculatorKey, { keyTypes } from './CalculatorKey/CalculatorKey';
 import DisplayComponent from '../../shared/components/Display/Display';
 import './styles.scss';
 
-
 class CalculatorComponent extends PureComponent {
   constructor(props) {
     super(props);
@@ -16,34 +15,54 @@ class CalculatorComponent extends PureComponent {
     this.setState({ display: '' });
   }
 
-  addToDisplay = val => {
-    this.setState(prevState => ({
-    display: prevState.display + val
-  }));
-  };
+  isOperator = (key) => {
+    const operators = ['+', '-', '*', '/'];
+    return operators.includes(key);
+  }
 
-  handleEqual = () => {
-    this.setState(prevState => ({
-      display : eval(prevState.display) 
-    }));
-  };
-
-  // changeOfOperator = () => {
-  //   const lastElem = this.state.display.slice(-1);
-  //   (lastElem === '-', '+', '/', '*') ? alert('error') : this.state.display;
-    
-
-  // }
-
-  changeOfOperator = () => {
+  checkLastElementOnDisplay = () => {
     const lastElem = this.state.display.slice(-1);
-    (3 === 3) ? console.log('aaaaa') : this.handleEqual;
-    
+    return this.isOperator(lastElem);
+  };
+
+  addNewElement = val => {
+    this.setState(prevState => ({
+      display: prevState.display + val,
+    }))
+  };
+
+  replaceLastElement = val => {
+    const display = this.state.display;
+    const displayReplaced = display.slice(0, -1) + val;
+    this.setState({
+      display: displayReplaced,
+    })
+  }
+
+  addToDisplay = (val) => {
+    // this.checkLastElementOnDisplay() ? this.replaceLastElement(val) : this.addNewElement(val)
+    if (this.isOperator(val)) {
+      this.checkLastElementOnDisplay(val) ? this.replaceLastElement(val) : this.addNewElement(val)
+    } else {
+        this.addNewElement(val)
+    }
+  }
+
+    handleEqual = () => {
+    const display = this.state.display;
+    const lastElem = display.slice(-1);
+    const operators = ['+', '-', '*', '/'];
+    if (operators.includes(lastElem) || display.length === 1) {
+      console.log('')
+    } else {
+        this.setState(prevState => ({
+          display : eval(prevState.display) 
+        }))
+    }
   }
   
-
   calculatorKeys = [
-    { value: 'C', type: keyTypes.clear,action: this.clearDisplay },
+    { value: 'C', type: keyTypes.clear, action: this.clearDisplay },
     { value: 7, action: this.addToDisplay },
     { value: 8, action: this.addToDisplay },
     { value: 9, action: this.addToDisplay },
@@ -58,8 +77,8 @@ class CalculatorComponent extends PureComponent {
     { value: '-', type: keyTypes.operator, action: this.addToDisplay },
     { value: 0, action: this.addToDisplay },
     { value: '.', action: this.addToDisplay },
-    { value: '=', type: keyTypes.result ,action: this.changeOfOperator },
-    { value: '+', type:keyTypes.operator, action: this.addToDisplay },
+    { value: '=', type: keyTypes.result, action: this.handleEqual },
+    { value: '+', type: keyTypes.operator, action: this.addToDisplay },
   ]
 
   render() {
@@ -75,6 +94,7 @@ class CalculatorComponent extends PureComponent {
             {
               this.calculatorKeys.map(({ value, type, action }) => (
                 <CalculatorKey
+                  key={value}
                   value={value}
                   type={type}
                   onPressed={action}
